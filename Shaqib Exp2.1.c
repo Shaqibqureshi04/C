8eea772e6903b508875f94c146f30de9193d40c7
Shaqibqureshi04 <qureshishaqib815@eng.rizvi.edu.in>
2024-10-22 21:30:42 +0530

    Add files via upload

---
+++ Shaqib Exp2.1.c
@@ -0,0 +1,34 @@
+/*
+EXPT :2.1
+NAME :Shaqib Qureshi 
+WAP : TO FIND THE NUMBER ARE EVEN OR ODD
+UIN :241S040
+BRANCH : COMPUTER DIV : E
+*/
+#include<stdio.h>
+
+int main()
+{
+ int n1;
+  {
+    printf("\t****CHECKING FOR EVEN ****\t \n");
+    printf("ENTER VALUE =\t");
+    scanf("%d",&n1);
+    }
+  if( n1%2==0)
+   {
+   printf("It's Even Number\n",&n1);
+   }
+  else 
+   {
+   printf("Its an Odd Number\n\a",&n1);
+   }
+ return 0;
+}
+/*
+OUTPUT:
+****CHECKING FOR EVEN ****
+ENTER VALUE =   5
+Its an Odd Number
+
+*/
