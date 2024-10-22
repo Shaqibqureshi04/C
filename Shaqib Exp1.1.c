52dd24f6f316342c9fc6b56227b420bc889f544f
Shaqibqureshi04 <qureshishaqib815@eng.rizvi.edu.in>
2024-10-22 21:29:55 +0530

    Add files via upload

---
+++ Shaqib Exp1.1.c
@@ -0,0 +1,31 @@
+/*Expt: 1.1
+Wap to print your name,age,class,division
+and UIN on the screen, use escape
+sequence to properly format the output
+
+Name: Shaqib Qureshi 
+UIN: 241S040
+Class: F.E ECS
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("name\t:\tShaqib Qureshi\n");
+    printf("age\t:\t18\n");
+    printf("class\t:\tF.E ECS\n");
+    printf("div\t:\tE\n");
+    printf("UIN\t:\t241S040\n");
+    
+
+    return 0;
+}
+
+/*output
+name    :       Shaqib Qureshi 
+age     :       18
+class   :       F.E Ecs
+div     :       E
+UIN     :       241S040
+*/
