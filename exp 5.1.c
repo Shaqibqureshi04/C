e557de26d3a7973756ff90424496451611520497
Shaqibqureshi04 <qureshishaqib815@eng.rizvi.edu.in>
2024-12-02 20:17:28 -0800

    Add files via upload

---
+++ exp 5.1.c
@@ -0,0 +1,74 @@
+
+
+Name : Shaqib qureshi
+Division: E
+UIN: 241S040
+exp5.1
+
+
+*/
+
+
+#include<stdio.h>
+//Factorial function int calcfact (int a)
+{
+int fact = 1;
+//calculating factorial
+for (int i=1; i<=a; i++)
+
+
+
+(
+fact fact*i;
+}
+
+
+return fact;
+//End of fact function
+
+}
+
+
+{
+
+
+//Main function
+int main()
+int x, result;
+printf("\t\t\t\t*** Calculate Factorial of Enter Number ***\n\n");
+printf("Enter a number : ");
+scanf("%d", &x);
+
+
+
+
+//Check if x>0
+//If condition is satisfied then execute IF
+if (x > 0)
+(
+}
+result calcfact (x); //Factorial function called//
+//Displaying result
+printf("\nThe Factorial Of %d is %d\n", x, result);
+printf("Thank youuuu For Using\n");
+else //If conditon is false then show Error//
+printf("\n Error: Enter number only\n");
+
+
+
+return 0;
+
+}
+
+
+/*OUTPUT
+
+*** Calculate Factorial of Enter Number ***
+
+
+Enter a number: 15
+
+
+
+The Factorial of 15 is 2004310016 Thank youuuu For Using
+
