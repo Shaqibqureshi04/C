633532672435dcff659985a5f1abb1c47d6f8942
Shaqibqureshi04 <qureshishaqib815@eng.rizvi.edu.in>
2024-12-02 20:20:04 -0800

    Add files via upload

---
+++ 10.2.c
@@ -0,0 +1,34 @@
+Name-shaqib qureshi
+UIN-241S040
+Div-E
+exp10.2
+
+#include <stdio.h>
+int main() {
+ int n;
+ printf("Enter the number of elements in the array: ");
+ scanf("%d", &n);
+ int arr[n];
+ int *ptr = arr;
+ printf("Enter %d elements:\n", n);
+ for (int i = 0; i < n; i++) {
+ scanf("%d", ptr + i);
+ }
+ printf("Array elements in reverse order:\n");
+ for (int i = n - 1; i >= 0; i--) {
+ printf("%d ", *(ptr + i));
+ }
+ printf("\n");
+ return 0;
+}
+/*output 
+Enter the number of elements in the array: 6
+Enter 6 elements:
+45
+55
+65
+75
+85
+95
+Array elements in reverse order:
+95 85 75 65 55 45
