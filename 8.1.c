be3e7f592d840560844ceebd44f8b817da2ee1b4
Shaqibqureshi04 <qureshishaqib815@eng.rizvi.edu.in>
2024-12-02 20:18:54 -0800

    Add files via upload

---
+++ 8.1.c
@@ -0,0 +1,20 @@
+Name-shaqib Qureshi 
+UIN-241S040
+Div-E
+exp8.1
+
+#include <stdio.h>
+int main() {
+ char str[100];
+ int length = 0;
+ printf("Enter a string: ");
+ scanf("%s", str);
+ while (str[length] != '\0') {
+ length++;
+ }
+ printf("The length of the string is: %d\n", length);
+ return 0;
+}
+/*output 
+Enter a string: 56
+The length of the string is: 2
